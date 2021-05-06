cd98519957925828f91d22e0fcb578dc13a5c99a
Ali Bala <balarabe@protonmail.com>
2021-05-06 16:28:43 +0100

    Create temp_test.go

---
+++ temp_test.go
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------------
+// github.com/balacode/udpt                                      /[temp_test.go]
+// (c) balarabe@protonmail.com                                      License: MIT
+// -----------------------------------------------------------------------------
+
+package udpt
+
+import (
+	"testing"
+)
+
+//
+// go test --run TestTemp
+//
+func TestTemp(t *testing.T) {
+	pl("TestTemp")
+} //                                                                    TestTemp
+
+// end
