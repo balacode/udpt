8b04c8d2bfc73305c31a54f9225a87635405fa21
Ali Bala <balarabe@protonmail.com>
2021-05-13 20:37:14 +0100

    Create unit test for makeError()

---
+++ make_error_test.go
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------
+// github.com/balacode/udpt                                /[make_error_test.go]
+// (c) balarabe@protonmail.com                                      License: MIT
+// -----------------------------------------------------------------------------
+
+package udpt
+
+import (
+	"testing"
+)
+
+// go test -run _makeError_
+//
+func Test_makeError_(t *testing.T) {
+	for _, it := range []struct {
+		id   uint32
+		args []interface{}
+		//
+		expect string
+	}{
+		{0, nil, "ERROR 0x000000:"},
+		{1, nil, "ERROR 0x000001:"},
+		{1, []interface{}{"a", 1, 2, "c", 3}, "ERROR 0x000001: a 1 2 c 3"},
+		{0xE12345, []interface{}{"failed"}, "ERROR 0xE12345: failed"},
+		{0xE12345, []interface{}{"failed", 123}, "ERROR 0xE12345: failed 123"},
+	} {
+		err := makeError(it.id, it.args...)
+		got := err.Error()
+		if got != it.expect {
+			t.Errorf("makeError(%X, %#v)"+
+				"\n expect: %#v"+
+				"\n    got: %#v",
+				it.id, it.args, it.expect, got)
+		}
+	}
+} //                                                             Test_makeError_
+
+// end
