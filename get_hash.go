5fcc699007168617890c3c2d5e86948960034b36
Ali Bala <balarabe@protonmail.com>
2021-05-18 22:51:24 +0100

    Extract getHashD() from getHash() to improve tests

--- get_hash.go
+++ get_hash.go
@@ -7,22 +7,28 @@ package udpt
 
 import (
 	"crypto/sha256"
+	"hash"
 )
 
 // getHash returns the SHA-256 hash of data as a slice of 32 bytes.
 func getHash(data []byte) []byte {
-	sha := sha256.New()
-	n, err := sha.Write(data)
+	return getHashD(data, sha256.New())
+} //                                                                     getHash
+
+// getHashD carries out the work for getHash(), and accepts
+// a dependency 'hs' that can be mocked during testing.
+func getHashD(data []byte, hs hash.Hash) []byte {
+	n, err := hs.Write(data)
 	if n != len(data) || err != nil {
 		// this should never happen
 		panic(makeError(0xE51EC0, err).Error())
 	}
-	ret := sha.Sum(nil)
+	ret := hs.Sum(nil)
 	if len(ret) != 32 {
 		// this should never happen
 		panic(makeError(0xE4D3E1, "invalid hash size").Error())
 	}
 	return ret
-} //                                                                     getHash
+} //                                                                    getHashD
 
 // end
