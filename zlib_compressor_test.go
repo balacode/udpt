f386152f4bedb9ec569e262dcae15c0608acb2d5
Ali Bala <balarabe@protonmail.com>
2021-05-19 15:45:17 +0100

    Improve zlibCompressor unit tests

--- zlib_compressor_test.go
+++ zlib_compressor_test.go
@@ -7,6 +7,7 @@ package udpt
 
 import (
 	"bytes"
+	"io"
 	"strings"
 	"testing"
 )
@@ -14,21 +15,155 @@ import (
 // go test -run _ZLibCompressor_
 //
 func Test_ZLibCompressor_(t *testing.T) {
-	zc := zlibCompressor{}
-	input := []byte(strings.Repeat(
-		"The quick brown fox jumps over the lazy dog!", 7,
-	))
-	comp, err := zc.Compress(input)
-	if err != nil {
-		t.Error("0xE26CD5 Compress() failed:", err)
+	input := []byte(
+		strings.Repeat("The quick brown fox jumps over the lazy dog!", 7),
+	)
+	// compresses input: must always succeed
+	compress := func() []byte {
+		zc := zlibCompressor{}
+		comp, err := zc.Compress(input)
+		if err != nil {
+			t.Error("0xE26CD5", err)
+		}
+		return comp
 	}
-	uncomp, err := zc.Uncompress(comp)
-	if err != nil {
-		t.Error("0xE38FD2 Uncompress() failed:", err)
+	{
+		// tests compressing and uncompressing data
+		comp := compress()
+		zc := zlibCompressor{}
+		uncomp, err := zc.Uncompress(comp) // must succeed
+		if err != nil {
+			t.Error("0xE38FD2", err)
+		}
+		if !bytes.Equal(input, uncomp) {
+			t.Error("0xEB0E80", "data corrupted")
+		}
 	}
-	if !bytes.Equal(input, uncomp) {
-		t.Error("0xEB0E80 Compress()/Uncompress() failed")
+	{
+		// uncompressing less than 4 bytes must fail
+		zc := zlibCompressor{}
+		comp, err := zc.Uncompress([]byte{1, 2, 3})
+		if comp != nil {
+			t.Error("0xEE55FC")
+		}
+		if !matchError(err, "invalid 'compressed'") {
+			t.Error("0xE4AB37")
+		}
+	}
+	{
+		// test wr.Write() failing in compressDI()
+		zc := zlibCompressor{}
+		cbuf := bytes.Buffer{}
+		wrc := &mockWriteCloser{failWrite: true}
+		comp, err := zc.compressDI(input, wrc, &cbuf)
+		if comp != nil {
+			t.Error("0xE27BB2")
+		}
+		if !matchError(err, "0xE12345: from mockWriteCloser.Write") {
+			t.Error("0xEE9C54", err)
+		}
+	}
+	{
+		// test wr.Close() failing in compressDI()
+		zc := zlibCompressor{}
+		cbuf := bytes.Buffer{}
+		wrc := &mockWriteCloser{failClose: true}
+		comp, err := zc.compressDI(input, wrc, &cbuf)
+		if comp != nil {
+			t.Error("0xE6F6A5")
+		}
+		if !matchError(err, "0xE12345: from mockWriteCloser.Close") {
+			t.Error("0xE4DF92", err)
+		}
+	}
+	{
+		// test reader.Read() failing so io.Copy() fails too in uncompressDI()
+		comp, zc := compress(), zlibCompressor{}
+		newMockReadCloser := func(io.Reader) (io.ReadCloser, error) {
+			return &mockReadCloser{failRead: true}, nil
+		}
+		uncomp, err := zc.uncompressDI(comp, newMockReadCloser)
+		if uncomp != nil {
+			t.Error("0xE3DA4F")
+		}
+		if !matchError(err, "0xE12345: from mockReadCloser.Read") {
+			t.Error("0xE81C62")
+		}
+	}
+	{
+		// test reader.Close() failing in uncompressDI()
+		comp, zc := compress(), zlibCompressor{}
+		newMockReadCloser := func(io.Reader) (io.ReadCloser, error) {
+			return &mockReadCloser{failClose: true}, nil
+		}
+		uncomp, err := zc.uncompressDI(comp, newMockReadCloser)
+		if uncomp != nil {
+			t.Error("0xEF3A01")
+		}
+		if !matchError(err, "0xE12345: from mockReadCloser.Close") {
+			t.Error("0xEA0F76")
+		}
 	}
 } //                                                        Test_ZLibCompressor_
+
+// -----------------------------------------------------------------------------
+
+// mockReadCloser is a mock io.ReadCloser with methods you can make fail.
+type mockReadCloser struct {
+	failRead  bool
+	failClose bool
+} //                                                              mockReadCloser
+
+// Read is a method of mockReadCloser implementing io.ReadCloser.
+//
+// You can make it return an error by setting mockReadCloser.failRead.
+//
+func (ob *mockReadCloser) Read(p []byte) (n int, err error) {
+	if ob.failRead {
+		return 0, makeError(0xE12345, "from mockReadCloser.Read")
+	}
+	return len(p), nil
+} //                                                                        Read
+
+// Close is a method of mockReadCloser implementing io.ReadCloser.
+//
+// You can make it return an error by setting mockReadCloser.failClose.
+//
+func (ob *mockReadCloser) Close() error {
+	if ob.failClose {
+		return makeError(0xE12345, "from mockReadCloser.Close")
+	}
+	return nil
+} //                                                                       Close
+
+// -----------------------------------------------------------------------------
+
+// mockWriteCloser is a mock io.WriteCloser with methods you can make fail.
+type mockWriteCloser struct {
+	failWrite bool
+	failClose bool
+} //                                                             mockWriteCloser
+
+// Write is a method of mockWriteCloser implementing io.WriteCloser.
+//
+// You can make it return an error by setting mockWriteCloser.failWrite.
+//
+func (ob *mockWriteCloser) Write(p []byte) (n int, err error) {
+	if ob.failWrite {
+		return 0, makeError(0xE12345, "from mockWriteCloser.Write")
+	}
+	return len(p), nil
+} //                                                                       Write
+
+// Close is a method of mockWriteCloser implementing io.WriteCloser.
+//
+// You can make it return an error by setting mockWriteCloser.failClose.
+//
+func (ob *mockWriteCloser) Close() error {
+	if ob.failClose {
+		return makeError(0xE12345, "from mockWriteCloser.Close")
+	}
+	return nil
+} //                                                                       Close
 
 // end
