acb44bd84856242eb8fc1ba4eda14476c555cd1f
Ali Bala <balarabe@protonmail.com>
2021-05-20 11:11:40 +0100

    Create more Configuration unit tests

--- config_test.go
+++ config_test.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"regexp"
 	"testing"
+	"time"
 )
 
 // to run all tests in this file:
@@ -29,24 +30,131 @@ func Test_config_NewDebugConfig_(t *testing.T) {
 		ret := string(rx.ReplaceAll([]byte(s), []byte("), ")))
 		return ret
 	}
-	// test!
-	got := NewDebugConfig()
-	gotS := formatStruct(got)
-	//
-	// debug configuration should match the one returned
-	// by NewDefaultConfig() but with logging activated
-	expect := NewDefaultConfig()
-	expect.VerboseSender = true
-	expect.VerboseReceiver = true
-	expect.LogFunc = LogPrint
-	expectS := formatStruct(expect)
-	//
-	if gotS != expectS {
-		t.Error(
-			"expect:", expectS, "\n",
-			"   got:", gotS,
-		)
+	{
+		// test!
+		got := NewDebugConfig()
+		gotS := formatStruct(got)
+		//
+		// debug configuration should match the one returned
+		// by NewDefaultConfig() but with logging activated
+		expect := NewDefaultConfig()
+		expect.VerboseSender = true
+		expect.VerboseReceiver = true
+		expect.LogFunc = LogPrint
+		expectS := formatStruct(expect)
+		//
+		if gotS != expectS {
+			t.Error(
+				"expect:", expectS, "\n",
+				"   got:", gotS,
+			)
+		}
+	}
+	{
+		isFuncCalled := false
+		logFunc := func(args ...interface{}) {
+			isFuncCalled = true
+		}
+		// test!
+		cfg := NewDebugConfig(logFunc)
+		cfg.LogFunc()
+		if !isFuncCalled {
+			t.Error("0xEE3AC7")
+		}
 	}
 } //                                                 Test_config_NewDebugConfig_
+
+// (ob *Configuration) Validate() error
+//
+// go test -run _config_Configuration_Validate_
+//
+func Test_config_Configuration_Validate_(t *testing.T) {
+	makeValidConfig := func() *Configuration {
+		return &Configuration{
+			//
+			// Components:
+			Cipher:     &aesCipher{},
+			Compressor: &zlibCompressor{},
+			//
+			// Limits:
+			PacketSizeLimit:   1450,
+			PacketPayloadSize: 1024,
+			SendBufferSize:    16 * 1024 * 2014, // 16 MiB
+			SendRetries:       10,
+			//
+			// Timeouts and Intervals:
+			ReplyTimeout:       15 * time.Second,
+			SendPacketInterval: 2 * time.Millisecond,
+			SendRetryInterval:  250 * time.Millisecond,
+			SendWaitInterval:   50 * time.Millisecond,
+			WriteTimeout:       15 * time.Second,
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.Cipher = nil
+		err := cf.Validate()
+		if !matchError(err, "nil Configuration.Cipher") {
+			t.Error("0xE65F82")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.Compressor = nil
+		err := cf.Validate()
+		if !matchError(err, "nil Configuration.Compressor") {
+			t.Error("0xE2CF8C")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.PacketSizeLimit = 8 - 1
+		err := cf.Validate()
+		if !matchError(err, "invalid Configuration.PacketSizeLimit") {
+			t.Error("0xE6E9BA")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.PacketSizeLimit = (65535 - 8) + 1
+		err := cf.Validate()
+		if !matchError(err, "invalid Configuration.PacketSizeLimit") {
+			t.Error("0xED50EB")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.PacketPayloadSize = 0
+		err := cf.Validate()
+		if !matchError(err, "invalid Configuration.PacketPayloadSize") {
+			t.Error("0xEA00A5")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.PacketSizeLimit = 1000
+		cf.PacketPayloadSize = 1000 - 200 + 1
+		err := cf.Validate()
+		if !matchError(err, "invalid Configuration.PacketPayloadSize") {
+			t.Error("0xEC92E8")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.SendBufferSize = -1
+		err := cf.Validate()
+		if !matchError(err, "invalid Configuration.SendBufferSize") {
+			t.Error("0xE2FF75")
+		}
+	}
+	{
+		var cf = makeValidConfig()
+		cf.SendRetries = -1
+		err := cf.Validate()
+		if !matchError(err, "invalid Configuration.SendRetries") {
+			t.Error("0xE0DE62")
+		}
+	}
+} //                                         Test_config_Configuration_Validate_
 
 // end
