43669a873788ab18ee0637d9ccc382f4d5e2e1a0
Ali Bala <balarabe@protonmail.com>
2021-05-22 12:03:57 +0100

    Don't string multiple error codes in makeError()
    Leave the message, but only use the most recent error code.

--- make_error.go
+++ make_error.go
@@ -7,15 +7,19 @@ package udpt
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 // makeError returns a new error value from joining id and args.
 // The ID is formatted as a 6-digit hex string. e.g. "0xE12345"
 func makeError(id uint32, args ...interface{}) error {
-	sID := fmt.Sprintf("ERROR 0x%06X:", id)
-	msg := joinArgs(sID, args...)
-	ret := fmt.Errorf(msg)
-	return ret
+	rx := regexp.MustCompile(`ERROR 0x[0-9a-fA-F]*: `)
+	msg := joinArgs("", args...)
+	msg = string(rx.ReplaceAll([]byte(msg), []byte("")))
+	msg = fmt.Sprintf("ERROR 0x%06X: ", id) + msg
+	msg = strings.TrimSpace(msg)
+	return fmt.Errorf(msg)
 } //                                                                   makeError
 
 // end
