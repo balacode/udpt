795e8733212a254b446e1f8dd8e325148e70a75e
Ali Bala <balarabe@protonmail.com>
2021-05-23 11:39:52 +0100

    Move mockNetAddr, mockNetUDPConn to assist_test.go

--- assist_test.go
+++ assist_test.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"strings"
 	"testing"
+	"time"
 )
 
 // makeTestConn creates a UDP connection for testing.
@@ -31,6 +32,60 @@ func matchError(err error, msg string) bool {
 	}
 	return err != nil && strings.Contains(err.Error(), msg)
 } //                                                                  matchError
+
+// -----------------------------------------------------------------------------
+
+// mockNetAddr is a mock net.Addr implementation which can
+// be made to return the network and address you want.
+type mockNetAddr struct {
+	network string
+	addr    string
+}
+
+// Network is the name of the network (e.g. "udp")
+func (mk *mockNetAddr) Network() string { return mk.network }
+
+// String form of the address (e.g. "127.0.0.1:9876")
+func (mk *mockNetAddr) String() string { return mk.addr }
+
+// -----------------------------------------------------------------------------
+
+// mockNetUDPConn is a mock net.UDPConn with methods you can make fail.
+type mockNetUDPConn struct {
+	failSetReadDeadline bool
+	failReadFrom        bool
+	failClose           bool
+} //                                                              mockNetUDPConn
+
+func (mk *mockNetUDPConn) ReadFrom(b []byte) (int, net.Addr, error) {
+	if mk.failReadFrom {
+		return 0, nil, makeError(0xED19BF, "failed SetReadDeadline")
+	}
+	addr := &mockNetAddr{network: "udp", addr: "127.8.9.10:11"}
+	return len(b), addr, nil
+} //                                                                    ReadFrom
+
+func (mk *mockNetUDPConn) WriteTo(b []byte, addr net.Addr) (int, error) {
+	return 0, nil
+} //                                                                     WriteTo
+
+func (mk *mockNetUDPConn) SetReadDeadline(time.Time) error {
+	if mk.failSetReadDeadline {
+		return makeError(0xED5A2C, "failed SetReadDeadline")
+	}
+	return nil
+} //                                                             SetReadDeadline
+
+func (mk *mockNetUDPConn) SetWriteDeadline(time.Time) error {
+	return nil
+} //                                                            SetWriteDeadline
+
+func (mk *mockNetUDPConn) Close() error {
+	if mk.failClose {
+		return makeError(0xE60D82, "failed Close")
+	}
+	return nil
+} //                                                                       Close
 
 // -----------------------------------------------------------------------------
 
