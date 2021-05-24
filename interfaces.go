89e7d19f4e2c5009ece79d3708849e79debc6950
Ali Bala <balarabe@protonmail.com>
2021-05-24 12:27:20 +0100

    Add Write() and SetWriteBuffer() to netUDPConn

--- interfaces.go
+++ interfaces.go
@@ -13,8 +13,10 @@ import (
 // netUDPConn specifies the interface of net.UDPConn as used in this package.
 type netUDPConn interface {
 	ReadFrom([]byte) (int, net.Addr, error)
+	Write(p []byte) (n int, err error)
 	WriteTo(b []byte, addr net.Addr) (int, error)
 	SetReadDeadline(time.Time) error
+	SetWriteBuffer(bytes int) error
 	SetWriteDeadline(time.Time) error
 	Close() error
 } //                                                                  netUDPConn
