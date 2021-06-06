bfd70de9ff76e92457880c55f57d676399bdbb70
Ali Bala <balarabe@protonmail.com>
2021-06-06 20:33:26 +0100

    Create demo2() to demonstrate using Receive()

--- demo/demo.go
+++ demo/demo.go
@@ -6,6 +6,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/balacode/udpt"
@@ -30,6 +31,32 @@ func main() {
 		fmt.Println("failed sending:", err)
 	}
 	rc.Stop()
+	defer demo2()
 } //                                                                        main
+
+// demo2 shows another way to run a Receiver, by using udpt.Receive function.
+func demo2() {
+	// secret encryption key shared by the Sender and Receiver
+	cryptoKey := []byte("aA2Xh41FiC4Wtj3e5b2LbytMdn6on7P0")
+	//
+	// run the receiver
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		err := udpt.Receive(ctx, 9876, cryptoKey,
+			func(k string, v []byte) error {
+				fmt.Println("Received k:", k, "v:", string(v))
+				return nil
+			})
+		if err != nil {
+			fmt.Println("failed receiving:", err)
+		}
+	}()
+	// send a message to the receiver
+	err := udpt.SendString("127.0.0.1:9876", "demo2", "Hello World!", cryptoKey)
+	if err != nil {
+		fmt.Println("failed sending:", err)
+	}
+	cancel()
+} //                                                                       demo2
 
 // end
